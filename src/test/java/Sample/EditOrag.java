2991f9b513d7bda0aad7efbe2d93922701bce2d7
Anbuit002 <Anbuit002@www.easeus.com>
2025-12-27 13:01:28 +0530

    hi Aravind adding new line

--- src/test/java/Sample/EditOrag.java
+++ src/test/java/Sample/EditOrag.java
@@ -3,13 +3,9 @@ package Sample;
 public class EditOrag {
 
 	public static void main(String[] args) {
-		System.out.println("Edit Ordg");
-		System.out.println("Edit Ordg");
-		System.out.println("Edit Ordg");
-		System.out.println("Edit Ordg");
-		System.out.println("Edit Ordg");
-		System.out.println("Edit Ordg");
+	
 		System.out.println(" hi Arvind");
+		System.out.println(" hi Ranjith");
 
 	}
 
